4d396aeb1dca09a67b7276c6fe47f09464893d1f
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-03 14:39:53 -0700

    completed layout of components on App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <section className='order-name'>
+        <OrderName />
+        <h2>Order for: {orderName}</h2>
+      </section>
+      <section className='order-images'>
+        <OrderImages />
+      </section>
+      <section className='order-dropdowns'>
+        <OrderDropdowns />
+      </section>
+      <section className='instructions-input'>
+        <InstructionForm />
+      </section>
+      <section className='instructions-list'>
+        <InstructionList />
+      </section>
     </div>
   );
 }
